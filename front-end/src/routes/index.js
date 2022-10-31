46c9871929d97af5d012483fe9250ad91e2d9065
Sérgio Vieira <sergiovieira.webb@gmail.com>
2022-10-31 12:14:20 -0300

    fix: requisito 5

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -3,14 +3,14 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import Products from '../pages/Products';
+import Customer from '../pages/Customer';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/login" component={ Login } />
       <Route exact path="/register" component={ Register } />
-      <Route exact path="/:host/products" component={ Products } />
+      <Route exact path="/customer/products" component={ Customer } />
       <Redirect exact from="/" to="/login" />
     </Switch>
 
