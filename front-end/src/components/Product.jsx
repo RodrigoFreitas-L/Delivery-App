a9575ed47c4b43bb4445e5270519b9131000be45
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-10-31 19:46:54 -0300

    Enviando pro github

---
+++ front-end/src/components/Product.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function Product({ product, totalPrice }) {
+  const [quantity, setQuantity] = useState(0);
+  const { id, name, price, urlImage } = product;
+
+  // const { totalPrice } = total;
+  console.log(totalPrice);
+
+  const addItem = (priceAdd, quantityAdd) => {
+    setQuantity(quantity + 1);
+    totalPrice(Number(priceAdd), quantityAdd);
+  };
+
+  const rmItem = (priceRm, quantityRm) => {
+    if (quantity === 0) return false;
+    setQuantity(quantity - 1);
+    totalPrice(Number(priceRm), quantityRm);
+  };
+
+  return (
+    <div key={ id }>
+      <p
+        data-testid={ `customer_products__element-card-title-${id}` }
+      >
+        {name}
+
+      </p>
+      <p
+        data-testid={ `customer_products__element-card-price-${id}` }
+      >
+        {price}
+
+      </p>
+      <img
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
+        src={ urlImage }
+        alt={ name }
+      />
+      <button
+        onClick={ () => addItem(price, quantity) }
+        data-testid={ `customer_products__button-card-add-item-${id}` }
+        type="button"
+      >
+        +
+
+      </button>
+      <input
+        onChange={ ({ target: { value } }) => setQuantity(value) }
+        value={ quantity }
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+        type="number"
+      />
+      <button
+        onClick={ () => rmItem(price, quantity) }
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+        type="button"
+      >
+        -
+
+      </button>
+    </div>
+  );
+}
+
+Product.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  urlImage: PropTypes.string,
+  price: PropTypes.number,
+}.isRequired;
+
+export default Product;
--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,25 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import Product from '../components/Product';
 import api from '../services/api';
 
 function Products() {
   const [products, setProducts] = useState([]);
-  const [quantity, setQuantity] = useState(0);
   const [total, setTotal] = useState(0);
 
   const totalPrice = (item, quantityP) => {
     setTotal(item * quantityP);
-  };
-
-  const addItem = (item, quantityA) => {
-    setQuantity(quantity + 1);
-    totalPrice(Number(item), quantityA);
-  };
-
-  const rmItem = (item, quantityR) => {
-    if (quantity === 0) return false;
-    setQuantity(quantity - 1);
-    totalPrice(Number(item), quantityR);
   };
 
   useEffect(() => {
@@ -34,47 +23,11 @@ function Products() {
     <div>
       <Header />
       { products.map((item) => (
-        <div key={ item.id }>
-          <p
-            data-testid={ `customer_products__element-card-title-${item.id}` }
-          >
-            {item.name}
-
-          </p>
-          <p
-            data-testid={ `customer_products__element-card-price-${item.id}` }
-          >
-            {item.price}
-
-          </p>
-          <img
-            data-testid={ `customer_products__img-card-bg-image-${item.id}` }
-            src={ item.urlImage }
-            alt={ item.name }
-          />
-          <button
-            onClick={ () => addItem(item.price, quantity) }
-            data-testid={ `customer_products__button-card-add-item-${item.id}` }
-            type="button"
-          >
-            +
-
-          </button>
-          <input
-            onChange={ ({ target: { value } }) => setQuantity(value) }
-            value={ quantity }
-            data-testid={ `customer_products__input-card-quantity-${item.id}` }
-            type="number"
-          />
-          <button
-            onClick={ () => rmItem(item.price, quantity) }
-            data-testid={ `customer_products__button-card-rm-item-${item.id}` }
-            type="button"
-          >
-            -
-
-          </button>
-        </div>
+        <Product
+          key={ item.id }
+          product={ item }
+          totalPrice={ totalPrice }
+        />
       )) }
       <h3>{total}</h3>
     </div>
